5666b77ec54bf0db8e6b9fc7f1d7658a93bd5d5b
Yusuke Minei <minei.yusuke2023@edtech.ryukyu>
2023-06-01 18:55:15 +0900

    initial commit

---
+++ src/test/java/com/example/MyBMS/MyBmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyBMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
